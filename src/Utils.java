46b83ea5416c28ad9174ab19def868717e93a777
kyle <kwoodc503@gmail.com>
2020-03-10 23:23:35 -0400

    sometimes you just gotta update those utils.java

--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 import javax.swing.DefaultListModel;
 
 public class Utils {
-//		Returns the classpath for json file
+//    Returns the classpath for json file
     public static String getClasspathDir() {
         String classpath = System.getProperty("java.class.path", ".");
         boolean windows = false;
@@ -27,34 +29,45 @@ public class Utils {
             }
             return classpathDirectory;
         }
-    }    
+    }
 
 //    Sorts DLM in ascending order
     public static DefaultListModel<String> sortWordsAsc(DefaultListModel<String> listOfWords) {
-    	String temp;
-    	int n = listOfWords.getSize();
-    	for (int i = 0; i < n; i++) {
-    		for (int j = i + 1; j < n; j++) {
+      String temp;
+      int n = listOfWords.getSize();
+      for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
                if ((listOfWords.get(i).compareTo(listOfWords.get(j)) > 0)) {
-            	   temp = listOfWords.get(i);
+                 temp = listOfWords.get(i);
                    listOfWords.set(i, listOfWords.get(j));
                    listOfWords.set(j, temp);
                }
            }
-    	}
-    	return listOfWords;
+      }
+      return listOfWords;
     }
 
-//		Reverse the order of a DLM
+//    Reverse the order of a DLM
     public static DefaultListModel<String> reverseOrder(DefaultListModel<String> words) {
-    	DefaultListModel<String> b = new DefaultListModel<String>(); 
-    	int n = words.getSize();
-    	int j = n; 
-    	for (int i = 0; i < n; i++) { 
-    		b.addElement(words.get(j-1)); 
-    		j = j - 1; 
-    	} 
-    	return b;
-    } 
+      DefaultListModel<String> b = new DefaultListModel<String>();
+      int n = words.getSize();
+      int j = n;
+      for (int i = 0; i < n; i++) {
+        b.addElement(words.get(j-1));
+        j = j - 1;
+      }
+      return b;
+    }
+    
+    public static ArrayList<Words> reverseOrderArrayList(ArrayList<Words> words) {
+        ArrayList<Words> b = new ArrayList<Words>();
+        int n = words.size();
+        int j = n;
+        for (int i = 0; i < n; i++) {
+          b.add(words.get(j-1));
+          j = j - 1;
+        }
+        return b;
+      }
 
 }
